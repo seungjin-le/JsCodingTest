c6286b4b8b5f8ed84f15bb52b82360cede3467ef
Leeseungjin <dltmdwls154@gmail.com>
2022-04-26 22:49:41 +0900

    문자열에 있는 모든문자들 숫자로 치환 하는 코딩 테스트

--- src/components/numStrEn.jsx
+++ src/components/numStrEn.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 const NumStrEn = () => {
+  function solution(s) {
+    const str = [
+      /zero/gi,
+      /one/gi,
+      /two/gi,
+      /three/gi,
+      /four/gi,
+      /five/gi,
+      /six/gi,
+      /seven/gi,
+      /eight/gi,
+      /nine/gi,
+    ];
+    str.map((v, i) => (s = s.replace(v, i)));
+    s = parseInt(s);
+    return s <= 0 ? 1 : s;
+  }
+  console.log(solution("2threethreethreethree45sixseventhreethree"));
   return <div></div>;
 };
 
