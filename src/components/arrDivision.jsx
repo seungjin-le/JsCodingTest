982167715e198558f60c2b84f154be16562bd2fc
Leeseungjin <dltmdwls154@gmail.com>
2022-04-28 19:55:05 +0900

    arrDivision Add

---
+++ src/components/arrDivision.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ArrDivision = () => {
+  const solution = (arr, divisor) => {
+    let answer = [];
+    arr.map((v, i) => v % divisor === 0 && answer.push(v));
+    answer.length !== 0 ? answer.sort((a, b) => a - b) : answer.push(-1);
+    return answer;
+  };
+  solution([5, 9, 7, 10], 5);
+  solution([2, 36, 1, 3], 1);
+  solution([3, 2, 6], 10);
+  return <div></div>;
+};
+
+export default ArrDivision;
