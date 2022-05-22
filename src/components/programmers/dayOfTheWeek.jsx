ec0ef31562463390157bf5552d2bf31697e1abea
Leeseungjin <dltmdwls154@gmail.com>
2022-05-22 21:30:16 +0900

    2016년 설명 추가

--- src/components/programmers/dayOfTheWeek.jsx
+++ src/components/programmers/dayOfTheWeek.jsx
@@ -4,8 +4,9 @@ import React from "react";
 
 const DayOfTheWeek = () => {
   const solution = (a, b) => {
+    // 요일의 데이터를 담은 배열
     let answer = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-
+    // 2016년 a월 b일을 리턴
     return answer[new Date(`2016-${a}-${b}`).getDay()];
   };
   solution(4, 15);
