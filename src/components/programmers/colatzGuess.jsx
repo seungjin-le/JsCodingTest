3fcc1484dbf44ed0db28e11711cb02bf1454436e
Leeseungjin <dltmdwls154@gmail.com>
2022-06-15 23:50:04 +0900

    콜라츠 추측 설명 추가

--- src/components/programmers/colatzGuess.jsx
+++ src/components/programmers/colatzGuess.jsx
@@ -4,12 +4,17 @@ import React from 'react';
 
 const ColatzGuess = () => {
   const solution = (num) => {
+    // num 이 1이 아닐때 실행
     if(num !== 1){
+      // 반복 횟수 가 500을 넘는다면 -1 리턴
       for(let a = 1; a <= 500; a++){
+        // 현제 num 이 짝수라면 나누기 2 홀수라면 3을 곱하고 + 1
         num =  num % 2 === 0 ? num/2 : num*3+1;
+        // 500번을 실행했다면 -1 리턴
         if(a === 500){
           return -1
         }
+        // num 이 1이 됬다면 반복 횟수 리턴
         if(num === 1){
           return a
         }
