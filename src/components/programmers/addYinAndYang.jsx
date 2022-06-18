35fe9ea421ba5e1384933daa9ed1f61085b33b88
Leeseungjin <dltmdwls154@gmail.com>
2022-06-18 21:57:56 +0900

    음양 더하기 설명 추가

--- src/components/programmers/addYinAndYang.jsx
+++ src/components/programmers/addYinAndYang.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 const AddYinAndYang = () => {
   const solution=(absolutes, signs)=> {
     let answer = 0;
+    // absolutes 를 map 함수를 이용해 순회 하면서 i(index)를 이용해 signs[i]가 true 면 answer 에 v를 더하고 false 면 v를 뺍니다.
     absolutes.map((v,i) => signs[i] ? answer+=v : answer-=v);
     return answer;
   }
