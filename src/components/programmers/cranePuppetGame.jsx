d99ca6a6356a245059e1fea1d1149c0d6a6f5769
Leeseungjin <dltmdwls154@gmail.com>
2022-06-20 21:24:56 +0900

    크레인 게임 설명 추가

--- src/components/programmers/cranePuppetGame.jsx
+++ src/components/programmers/cranePuppetGame.jsx
@@ -5,11 +5,15 @@ import React from 'react';
 const CranePuppetGame = () => {
   const solution = (board, moves) => {
     let answer = 0;
+    // 이차원(board) 배열의 같은 인덱스 끼리 묶어 새로운 배열 생성
     let arr = [];
+    // 뽑은 인형을 담을 바구니 배열
     let basket = []
+    // 이차원(board) 배열의 특정 인덱스 값(a)만 묶어서 arr 에 넣어줌
     for(let a = 0; a < board.length; a++){
       arr[a] = board.map((v) => v[a])
     }
+    // 크레인 게임 로직
     for(let a = 0; a < moves.length; a++){
       for(let b = 0; b < arr.length; b++){
         if(arr[moves[a]-1][b]) {
@@ -19,6 +23,7 @@ const CranePuppetGame = () => {
         }
       }
     }
+    // 중복 찾기
     for(let a = 0; a < basket.length; a++){
       if(basket[a] === basket[a+1]){
         answer +=2
