3559c3754255755d9f341b53c198e32230240f8a
Leeseungjin <dltmdwls154@gmail.com>
2022-06-21 20:24:47 +0900

    다트게임 설명 추가

--- src/components/programmers/dartGame.jsx
+++ src/components/programmers/dartGame.jsx
@@ -5,16 +5,22 @@ import React from 'react';
 const DartGame = () => {
   const solution = (dartResult) => {
     let answer = 0;
+    // 점수를 담을 배열
     let score = []
+    // dartResult 한글자씩 잘라서 담을 새로운 배열.
     let arr = dartResult.split('')
     arr.map((v,i) => {
       if(+v){
+        // v 가 1일경우 v 가 10일수도 있으니 arr[i+1]로 10인지 확인
         if(+v+arr[i+1] === '10'){
           if(arr[i+2] === 'S'){
+            // arr[i+2]가 S 면 Math.pow 메소드로 1을 제곱후 score 에 push
             score.push(Math.pow(+v+arr[i+1],1))
           }else if(arr[i+2] === 'D'){
+            // arr[i+2]가 S 면 Math.pow 메소드로 2을 제곱후 score 에 push
             score.push(Math.pow(+v+arr[i+1],2))
           }else if(arr[i+2] === 'T'){
+            // arr[i+2]가 S 면 Math.pow 메소드로 3을 제곱후 score 에 push
             score.push(Math.pow(+v+arr[i+1],3))
           }
         }else{
@@ -28,8 +34,10 @@ const DartGame = () => {
         }
       }else{
         +v === 0 && +arr[i-1] !== 1 && score.push(+v)
+        // 스타상이나 야차상 일 경우 조건문 실행
         if(v === '#' || v === '*'){
           if(score.length === 1){
+            // 위 조건문을 통과 했으니 v는 스타상이나 야차상 둘중 하나라는 것이니 *이면 score * 2 아닐경우 -로 치환
             v === '*' ? score[0] = score[0]*2 : score[0] = Number(-score[0])
           }else if(score.length === 2){
             v === '*' ? (score[0] = score[0]*2,score[1] = score[1]*2) : score[1] = Number(-score[1])
@@ -39,6 +47,7 @@ const DartGame = () => {
         }
       }
     })
+    // reduce 함수로 배열의 값들을 전부 더해 answer 에 담아서 리턴
     answer = score.reduce((prev, curr) => prev + curr);
 
     return answer;
