295b6c0b95d7b3512b55dfc39c4a478612739783
Leeseungjin <dltmdwls154@gmail.com>
2022-06-26 00:10:31 +0900

    짝지어 제거하기 설명 추가

--- src/components/programmers/levelTwo/pairToRemove.jsx
+++ src/components/programmers/levelTwo/pairToRemove.jsx
@@ -4,14 +4,19 @@ import React from 'react';
 
 const PairToRemove = () => {
   const solution = (s) => {
+    //  빈배열 선언
     let arr = []
+    //  s의 길이만큼 반복
     for(let a = 0; a < s.length; a++){
+      //  배열 arr 의 마지막 요소가 s[a]와 다를경우 arr 에 push
       if(arr[arr.length-1] !== s[a] ){
         arr.push(s[a]);
       }else{
+        //  같을 경우 arr 의 마지막 요소 제거
         arr.pop()
       };
     }
+    //  arr 의 길이가 0 일경우 1을 반환하고 아니면 0을 반환
     return arr.length === 0 ? 1 : 0;
   }
   return (
