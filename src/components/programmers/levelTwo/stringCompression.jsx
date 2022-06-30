f63eab1fc9463d814722002bee3cda2fa3b6c6af
Leeseungjin <dltmdwls154@gmail.com>
2022-06-30 23:40:42 +0900

    문자열 압축 LV.2 다시 풀기위해 초기화

--- src/components/programmers/levelTwo/stringCompression.jsx
+++ src/components/programmers/levelTwo/stringCompression.jsx
@@ -5,28 +5,6 @@ import React from 'react';
 const StringCompression = () => {
   const solution = (s) => {
     let answer = 0;
-    let arr = []
-    const counterFun = (arr) => {
-      let count = 0
-      let counter = []
-      arr.map((v,i) => v === arr[i+1] &&
-        counter[count].push(v)
-      )
-      console.log(counter)
-      return[];
-    }
-    for(let q = 0,b=0,c=2; q< s.length-1/2; q++){
-      let testArr = []
-      for(let a = 0; a <= s.length-1/c; a++){
-        testArr.push(s.substr(b,c));
-        b+=c;
-      }
-      b=0,c++;
-      arr.push(testArr.filter((v) => v))
-    }
-    arr = arr.filter((v) => v.length > 1);
-    counterFun(arr[0])
-    //console.log(arr)
     return answer;
   }
   return (
