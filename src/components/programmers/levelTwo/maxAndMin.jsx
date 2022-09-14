77d6c0c16169ece7b7a55f9688d5733b4575ea76
Seungjin Lee <dltmdwls154@gmail.com>
2022-09-14 20:22:47 +0900

    JadenCase 문자열 만들기 추가

--- src/components/programmers/levelTwo/maxAndMin.jsx
+++ src/components/programmers/levelTwo/maxAndMin.jsx
@@ -6,7 +6,7 @@ const MaxAndMin = () => {
   const solution = (s) => {
     // 문자열 안에 숫자를 공백으로 나눴으니 공백을 기준으로 잘라 배열을 만들어줍니다.
     s = s.split(" ").map((v) => parseInt(v));
-    // 탬플릿 리터럴로 반환
+    // 템플릿 리터럴로 반환
     return `${Math.min(...s)} ${Math.max(...s)}`;
   };
   return <div></div>;
