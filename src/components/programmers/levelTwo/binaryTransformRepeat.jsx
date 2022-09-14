46a328c52fb4cfe15c536b3739db328b2322bac5
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-14 21:30:47 +0900

    이진 변환 반복 설명 추가, 로직 수정

--- src/components/programmers/levelTwo/binaryTransformRepeat.jsx
+++ src/components/programmers/levelTwo/binaryTransformRepeat.jsx
@@ -4,18 +4,20 @@ import React from "react";
 
 const BinaryTransformRepeat = () => {
   const solution = (s) => {
-    // 삭제한 0의 개수
-    let afterLen = 0;
-    // 이진 변환 반복 횟수
-    let count = 0;
+    // [ 이진 변환 반복 횟수, 삭제한 0의 개수 ]
+    let answer = [0, 0];
+    // s가 '1'이 될 때까지 반복
     while (s !== "1") {
+      // 0을 제거하기 전 길이
       let beforeLen = s.length;
       s = s.replace(/0/g, "");
-      afterLen += beforeLen - s.length;
+      // 0을 제거하기 전 길이 - 제거한 후 길이
+      answer[1] += beforeLen - s.length;
+      // 0을 제거한 후의 길이를 이진법으로 변환
       s = s.length.toString(2);
-      count++;
+      answer[0]++;
     }
-    return [count, afterLen];
+    return answer;
   };
   return <div></div>;
 };
