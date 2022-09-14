da10a53af24b7d44b0b009f9a11a7a00bcd10296
Seungjin Lee <dltmdwls154@gmail.com>
2022-09-14 23:28:23 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 function App() {
   return (
     <div>
-      {/*
-      신고한 유저와 당한유저
-      <UserReports />
-      */}
-      {/*<FizzBuzz />*/}
-      {/*
-      부족한돈 계산
-      <Insufficient />
-      */}
-      {/*<SamNumber />*/}
-      {/*
-      문자열안 문자를 숫자로 치환
-      <NumStrEn />*/}
-      {/*
-      <UndefinedNum />*/}
-      {/*
-      <FailureRate />*/}
-      {/*<String />*/}
-      {/*<SecretMap />*/}
-      {/* <FunctionDevelopment /> */}
     </div>
   );
 }
