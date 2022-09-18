c3b5c1fc54dd673c5f3115cc8e864b4dc7947a6b
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-18 23:28:24 +0900

    다음 큰 숫자 추가

--- src/components/programmers/levelTwo/nextBigNumber.jsx
+++ src/components/programmers/levelTwo/nextBigNumber.jsx
@@ -3,6 +3,15 @@ import React from 'react';
 // 다음 큰 숫자
 
 const NextBigNumber = () => {
+
+  const solution = (n) => {
+    let answer = n+1;
+    while(answer.toString(2).replace(/0/g,'') !== n.toString(2).replace(/0/g,'')){
+      answer++
+    }
+    return answer;
+  }
+
   return (
     <div>
 
