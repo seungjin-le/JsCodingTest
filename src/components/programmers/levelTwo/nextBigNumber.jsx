73addbe9b5bb4be1b2781410ced9b825c8358963
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-18 23:32:56 +0900

    다음 큰 수 설명 추가

--- src/components/programmers/levelTwo/nextBigNumber.jsx
+++ src/components/programmers/levelTwo/nextBigNumber.jsx
@@ -5,10 +5,13 @@ import React from 'react';
 const NextBigNumber = () => {
 
   const solution = (n) => {
+    // n보다 커야하니 시작은 n + 1
     let answer = n+1;
+    // n과 answer을 2진수로 변환하고 0을 제거했을때 같을때까지 answer++ 반복
     while(answer.toString(2).replace(/0/g,'') !== n.toString(2).replace(/0/g,'')){
       answer++
     }
+    // answer 리턴
     return answer;
   }
 
