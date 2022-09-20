8d479c4e2a83ca0981c13553f33b5f772eb40a2d
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-20 19:53:54 +0900

    타겟 넘버 설명 추가

--- src/components/programmers/levelTwo/targetNumber.jsx
+++ src/components/programmers/levelTwo/targetNumber.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
+// 타겟 넘버
+
 const TargetNumber = () => {
   const solution = (numbers, target) => {
     let answer = 0;
+    // 함수 counts에 매개변수가 주어지지 않았을 경우 기본값 0
     const counts = (count = 0, sum = 0) => {
+      // count가 numbers의 길이와 같아졌을경우
       if (count === numbers.length) {
+        // 반복해서 더하고 뺀sum가 target과 같을경우 answer + 1 한다음 함수 종료
         if (target === sum) {
           answer++;
         }
         return;
       }
-
+      // count가 numbers의 길이와 다를 경우 sum 과 numbers[count]를
+      // 더하거나 빼도록 2개의 함수 실행
       counts(count + 1, sum + numbers[count]);
       counts(count + 1, sum - numbers[count]);
     };
