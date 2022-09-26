a722c5d287b7f86b61ff6d0ef8e93ba4a562e8b1
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-26 18:55:30 +0900

    N개의 최소공배수 설명 추가

--- src/components/programmers/levelTwo/leastCommonMultipleOfNCrabs.jsx
+++ src/components/programmers/levelTwo/leastCommonMultipleOfNCrabs.jsx
@@ -5,18 +5,27 @@ import React from 'react';
 const LeastCommonMultipleOfNCrabs = () => {
   const solution = (arr) => {
     let answer = 0;
-    let start = Math.max(...arr);
+    // 배열안에 있는 모든 수들의 공배수를 구해야 하니 가장 큰수를 찾아 변수에 넣습니다.
+    let max = Math.max(...arr);
+    // 계속해서 가장 큰수를 더해줘야하니 가장 크수부터 시작
+    let count = max;
+
+    // 반복문 안에 있는 조건문을 통과시 answer++, answer은 arr의 길이와 같으면 종료
     while (answer !== arr.length) {
       arr.map((v) => {
-        if (start % v === 0) {
+        // count를 arr안에 있는 값들로 나눴을때 0일때마다 answer++
+        if (count % v === 0) {
           answer++;
         }
         return v;
       });
+      // answer은 arr의 길이와 같으면 현제 공배수를 담은 count를 리턴
       if (answer === arr.length) {
-        return start;
+        return count;
       }
-      start++;
+      // count에 가장 큰수를 계속 더해줍니다.
+      count += max;
+      // 반복문이 끝날때마다 answer을 초기화
       answer = 0;
     }
   };
