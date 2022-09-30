901f53668b1ddebc516c249955edb0345d4193f7
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-30 21:35:04 +0900

    오픈 채팅방 설명 수정

--- src/components/programmers/levelTwo/openChatRoom.jsx
+++ src/components/programmers/levelTwo/openChatRoom.jsx
@@ -11,8 +11,8 @@ const OpenChatRoom = () => {
     // 유저들의 정보와 기록이 담겨있는 배열을 이차원 배열로 변경
     record = record.map((v) => v.split(' '));
     // record = [
-    //   [(Enter or Leave or Change), (id), (Nick Name)],
-    //   [(Enter or Leave or Change), (id), (Nick Name)]
+    //   [(Enter or Leave or Change), (id), (nickName)],
+    //   [(Enter or Leave or Change), (id), (nickName)]
     // ]
 
     // 유저들의 정보가 담긴 이차원배열을 순회하면서 obj안에 유저id라는 객체를 만들고
@@ -34,7 +34,7 @@ const OpenChatRoom = () => {
     // 객체안에 유저의 정보 등록, 수정이 끝나면
     // 유저의 정보가 담긴 이차원 배열을 순회하면서
     // 이차원 배열의 첫번째 값이 'Change' 아니라면 answer에 메시지를 push
-    // v = [ v[0] = (Enter, Leave, Change), v[1] = uid, v[2] = nickName ]
+    // v = [ v[0] = (Enter, Leave, Change), v[1] = id, v[2] = nickName ]
     record.map((v) => v[0] !== 'Change' && answer.push(obj[v[1]][v[0]]));
     console.log(answer);
     return answer;
