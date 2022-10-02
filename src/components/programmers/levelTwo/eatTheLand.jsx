7bd4aa3f298339f7431470f01636c70b76582c4f
LeeSeungJin <dltmdwls154@gmail.com>
2022-10-02 20:26:26 +0900

    땅따먹기 풀이 추가

--- src/components/programmers/levelTwo/eatTheLand.jsx
+++ src/components/programmers/levelTwo/eatTheLand.jsx
@@ -3,6 +3,17 @@ import React from 'react';
 // 땅따먹기
 
 const EatTheLand = () => {
+  const solution = (land) => {
+    for (let a = 1; a < land.length; a++) {
+      for (let b = 0; b < land[0].length; b++) {
+        land[a][b] += Math.max(
+          ...land[a - 1].slice(0, b),
+          ...land[a - 1].slice(b + 1)
+        );
+      }
+    }
+    return Math.max(...land[land.length - 1]);
+  };
   return <div></div>;
 };
 
