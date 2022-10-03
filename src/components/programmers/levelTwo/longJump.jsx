c7fe2182566694bdacfa69412599559cf9581a48
LeeSeungJin <dltmdwls154@gmail.com>
2022-10-03 19:43:19 +0900

    멀리뛰기 변수명 수정

--- src/components/programmers/levelTwo/longJump.jsx
+++ src/components/programmers/levelTwo/longJump.jsx
@@ -6,7 +6,7 @@ const LongJump = () => {
   const solution = (n) => {
     let answer = 0,
       arr = [0, 1];
-    for (let c = 2; c <= n + 1; c++) {
+    for (let a = 2; a <= n + 1; a++) {
       answer = arr[0] + (arr[1] % 1234567);
       arr[0] = arr[1];
       arr[1] = answer;
