94403efa50c5cdaa5e9c940d2f76a5b467da6146
LeeSeungJin <dltmdwls154@gmail.com>
2022-10-10 18:35:25 +0900

    예상 대진표 추가

---
+++ src/components/programmers/levelTwo/expectTable.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// 예상 대진표
+
+const ExpectTable = () => {
+  const solution = (n, a, b) => {
+    let answer = 0;
+    while (a !== b) {
+      a = Math.ceil(a / 2);
+      b = Math.ceil(b / 2);
+      answer++;
+    }
+    return answer;
+  };
+
+  return <div></div>;
+};
+
+export default ExpectTable;
