3a6d150598fd0fbb4d02c69f98f7cf013e700314
Seungjin Lee <dltmdwls154@gmail.com>
2023-02-13 23:36:26 +0900

    1Day 1Commit

--- src/components/programmers/levelOne/ACodeForTwo.jsx
+++ src/components/programmers/levelOne/ACodeForTwo.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const ACodeForTwo = () => {
+  const solution = (s, skip, index) => {
+    let obj = {
+      skip: [],
+      texts: [],
+    };
+    if (s) {
+      for (let a = 0; a < s.length; a++) {
+        console.log(s.charCodeAt(a));
+      }
+    }
+    return;
+  };
   return <div></div>;
 };
 
