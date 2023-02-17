2727d2786a8f2b7d67719822b67b9db33148df57
Seungjin Lee <dltmdwls154@gmail.com>
2023-02-18 06:20:05 +0900

    크기가 작은 부분분자열 설명 추가

--- src/components/programmers/levelOne/smallSubstring.jsx
+++ src/components/programmers/levelOne/smallSubstring.jsx
@@ -4,8 +4,12 @@ import React from 'react';
 
 const SmallSubstring = () => {
   const solution = (t, p) => {
-    var answer = 0;
+    // 작은수를 카운트할 변수
+    let answer = 0;
+    // t의 길이 - p의 길이 +1 만큼 반복
     for (let a = 0; a < t.length - p.length + 1; a++) {
+      // 한자리씩 이동한다음 p의 길이만큼 잘라서 비교후
+      // p가 크거나 같으면 answer++
       +p >= +t.substr(a, p.length) && answer++;
     }
 
