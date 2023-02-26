34c94ee5ca4ece4968a7849a3401f7654725db29
Seungjin Lee <dltmdwls154@gmail.com>
2023-02-27 07:15:21 +0900

    가장 가까운 글자 풀이 추가

--- src/components/programmers/levelOne/theNearestSameLetter.jsx
+++ src/components/programmers/levelOne/theNearestSameLetter.jsx
@@ -3,6 +3,23 @@ import React from 'react';
 // 가장 가까운 같은 글자
 
 const TheNearestSameLetter = () => {
+  const solution = (s) => {
+    let arr = s.split('').reverse();
+    return arr
+      .map((v, i) => {
+        let index = -1;
+        let count = 0;
+        for (let a = i + 1; a < s.length || index !== -1; a++) {
+          count++;
+          if (v === arr[a]) {
+            index = count;
+            return index;
+          }
+        }
+        return index;
+      })
+      .reverse();
+  };
   return <div></div>;
 };
 
