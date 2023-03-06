f59f2ac837b04cb4ea9fd3a79033a8b2d25a892a
Seungjin Lee <dltmdwls154@gmail.com>
2023-03-07 05:28:54 +0900

    명예의 전당 풀이 추가

--- src/components/programmers/levelOne/theHallOfFame.jsx
+++ src/components/programmers/levelOne/theHallOfFame.jsx
@@ -3,6 +3,20 @@ import React from 'react';
 // 명예의 전당(1)
 
 const TheHallOfFame = () => {
+  function solution(k, score) {
+    let answer = [];
+
+    return score.map((v) => {
+      answer.push(v);
+      if (answer.length > k) {
+        answer = answer.sort((a, b) => b - a);
+        answer.pop();
+      }
+      const min = Math.min(...answer);
+
+      return min;
+    });
+  }
   return <div></div>;
 };
 
