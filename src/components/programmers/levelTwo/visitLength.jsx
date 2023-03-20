2d9832e02954d98f05484e3ed7753c05607cf12a
Seungjin Lee <dltmdwls154@gmail.com>
2023-03-20 21:16:39 +0900

    방문길이 코드 수정

--- src/components/programmers/levelTwo/visitLength.jsx
+++ src/components/programmers/levelTwo/visitLength.jsx
@@ -3,41 +3,39 @@ import React from 'react';
 // 방문 길이
 
 const VisitLength = () => {
-  function solution(dirs) {
+  const solution = (dirs) => {
     let arr = [[0, 0, 0, 0]];
     let obj = {
       x: [],
       y: [],
     };
-
-    dirs.split('').map((v, i) => {
+    const str = (pos, line) => {
+      return JSON.stringify([pos, ...line.sort((a, b) => a - b)]);
+    };
+    dirs.split('').map((v) => {
       let start = arr[arr.length - 1].slice(2, 4);
       let end = [...start];
       if (v === 'U' && start[1] < 5) end[1]++;
       if (v === 'D' && start[1] > -5) end[1]--;
       if (v === 'L' && start[0] > -5) end[0]--;
       if (v === 'R' && start[0] < 5) end[0]++;
-      if (JSON.stringify(start) === JSON.stringify(end)) return;
+      if (JSON.stringify(start) === JSON.stringify(end)) return null;
+
       if (v === 'U' || v === 'D') {
-        const line = JSON.stringify([
-          start[0],
-          ...[start[1], end[1]].sort((a, b) => a - b),
-        ]);
+        const line = str(start[0], [start[1], end[1]]);
         !obj.x.includes(line) && obj.x.push(line);
       }
       if (v === 'L' || v === 'R') {
-        const line = JSON.stringify([
-          start[1],
-          ...[start[0], end[0]].sort((a, b) => a - b),
-        ]);
+        const line = str(start[1], [start[0], end[0]]);
         !obj.y.includes(line) && obj.y.push(line);
       }
       arr.push([...start, ...end]);
     });
 
     return obj.x.length + obj.y.length;
-  }
+  };
   return <div></div>;
 };
 
 export default VisitLength;
+//pos.splice(0, 2, ...pos.slice(2,4))
