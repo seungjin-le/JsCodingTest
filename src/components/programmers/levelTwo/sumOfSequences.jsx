6674291cb3f277b3ad9014ccd5bff2dbf1fe7b36
Seungjin Lee <dltmdwls154@gmail.com>
2023-04-09 10:44:35 +0900

    연속된 부분 수열의 합 추가

--- src/components/programmers/levelTwo/sumOfSequences.jsx
+++ src/components/programmers/levelTwo/sumOfSequences.jsx
@@ -3,27 +3,27 @@ import React from 'react';
 // 연속된 부분 수열의 합
 
 const SumOfSequences = () => {
-  function solution(sequence, k) {
-    let arr = [];
-    let pos = [];
-    sequence
-      .filter((v) => v <= k)
-      .map((v, i) => {
-        arr.push(v);
-        let total = arr.reduce((a, b) => a + b);
-        //    total > k ? total === k ?
-      });
-    //sequence
-    for (let a = 0; a < sequence.length; a++) {
-      let count = 0;
-      for (let b = a; count < k; b++) {
-        count += sequence[b];
-        if (count === k) arr.push([a, b]);
-      }
-    }
-    return arr.sort((a, b) => a[1] - a[0] - (b[1] - b[0]))[0];
-  }
+  const solution = (sequence, k) => {
+    let answer = [];
+    let arr = [0];
+    let size = sequence.length;
+    let pos = [0, 0];
+    sequence.map((v, i) => arr.push(v + arr[i]));
 
+    while (pos[0] <= pos[1]) {
+      let sum = arr[pos[1]] - arr[pos[0]];
+      if (sum === k) {
+        let newSize = pos[1] - 1 - pos[0];
+        if (size > newSize) {
+          answer = [pos[0], pos[1] - 1];
+          size = newSize;
+        }
+      }
+      sum < k ? pos[1]++ : pos[0]++;
+    }
+
+    return answer;
+  };
   return <div></div>;
 };
 
