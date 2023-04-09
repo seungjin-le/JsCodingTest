ff4c67066e87f7568ae5035bd469b491ad2b3513
Seungjin Lee <dltmdwls154@gmail.com>
2023-04-10 06:45:22 +0900

    롤케이크 자르기 정답 추가

--- src/components/programmers/levelTwo/cuttingTheRollCake.jsx
+++ src/components/programmers/levelTwo/cuttingTheRollCake.jsx
@@ -3,31 +3,24 @@ import React from 'react';
 // 롤케이크 자르기
 
 const CuttingTheRollCake = () => {
-  function solution(topping) {
+  const solution = (topping) => {
+    let arr = [Array(10001).fill(0), Array(10001).fill(0)];
+    let set = [0, 0];
     let answer = 0;
-    let set = [...new Set(topping)]
-    let obj = {
-      arr: topping.splice(0,topping.indexOf(set[Math.ceil(1/2)+1])),
-      arrSize:0,
-      toppingSize:0
-    }
-    let as = [[],[]]
-    obj.arrSize = [...new Set(obj.arr)].length
-    obj.toppingSize = [...new Set(topping)].length
-    for(let a = 0 a < topping.length){
-      as[0].push(topping.shift())
-      as[1].push(topping.pop())
-    }
-    while(obj.arrSize <= obj.toppingSize){
-      let a = topping.shift()
-      if(!topping.includes(a)) obj.toppingSize--;
-      if(!obj.arr.includes(a)) (obj.arrSize++,obj.arr.push(a));
-      if(obj.arrSize === obj.toppingSize) answer++;
-      if(obj.arrSize > obj.toppingSize) return answer;
-    }
+    topping.map((v) => arr[1][v]++);
 
+    set[1] = arr[1].filter((v) => v !== 0).length;
+
+    topping.map((v) => {
+      if (arr[0][v] === 0) set[0]++;
+      arr[0][v]++;
+      arr[1][v]--;
+      if (arr[1][v] === 0) set[1]--;
+      if (set[0] === set[1]) answer++;
+      if (set[0] > set[1]) return answer;
+    });
     return answer;
-  }
+  };
   return <div></div>;
 };
 
